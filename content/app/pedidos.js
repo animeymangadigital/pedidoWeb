d51f505e8084c55cb8b4f80a9154a05ed7b43d8b
Xavier <xsarmiento@cebroker.com>
2016-11-17 15:35:47 -0500

    download excel

--- content/app/pedidos.js
+++ content/app/pedidos.js
@@ -29,7 +29,18 @@ var ViewModel = function() {
         self.showProds(false);
     };
     self.download = function() {
-      $('#pedidoTableGenerate').tableExport({type:'excel',escape:'false'});  
+      //Creamos un Elemento Temporal en forma de enlace
+        var tmpElemento = document.createElement('a');
+        // obtenemos la información desde el div que lo contiene en el html
+        // Obtenemos la información de la tabla
+        var data_type = 'data:application/vnd.ms-excel';
+        var tabla_div = document.getElementById('pedidoTableGenerate');
+        var tabla_html = tabla_div.outerHTML.replace(/ /g, '%20');
+        tmpElemento.href = data_type + ', ' + tabla_html;
+        //Asignamos el nombre a nuestro EXCEL
+        tmpElemento.download = 'Nombre_De_Mi_Excel.xls';
+        // Simulamos el click al elemento creado para descargarlo
+        tmpElemento.click();
     };
 
     self.calculateTotal = function(data) {
